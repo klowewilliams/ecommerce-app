229e3619a5336d542dfd3b5cd247b160a76a4063
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-06-08 20:29:59 -0500

    Updated db with Supplier model and attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-
+  belongs_to :supplier
+  
   def sale_message
     if price < 2
       return " Discount Item"
---
+++ db/migrate/20150609004853_add_new_supplier_id_to_products.rb
@@ -0,0 +1,5 @@
+class AddNewSupplierIdToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :supplier_id, :integer
+  end
+end
