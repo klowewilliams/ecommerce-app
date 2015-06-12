adb43f10f7f4d7f63bc0693f9f97a9cb59fa17da
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-06-11 19:13:59 -0500

    Added order details to order page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,13 @@ class OrdersController < ApplicationController
   end
 
   def create
-    order = Order.create(quantity: params[:quantity], product_id: params[:product_id], user_id: current_user.id)
+    quantity = params[:quantity].to_i
+    price = Product.find_by(id: params[:product_id]).price
+    subtotal = quantity * price
+    tax = subtotal * 0.09
+    total = subtotal + tax
+
+    order = Order.create(quantity: params[:quantity], product_id: params[:product_id], user_id: current_user.id, subtotal: subtotal, tax: tax, total_price: total)
     redirect_to "/orders/#{order.id}"
   end
 
