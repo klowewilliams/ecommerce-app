fab3c35ea4a3f262ff117ba28235bb2a7031f8cb
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-06-17 17:48:21 -0500

    changed migration of products

--- db/migrate/20150529004428_create_products.rb
+++ db/migrate/20150529004428_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
-      t.decimal :price, precision: 10, scale: 2
+      t.decimal :price, :precision => 10, :scale => 2
       t.string :image
       t.string :description
 
