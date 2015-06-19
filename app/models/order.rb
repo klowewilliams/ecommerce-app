1866223ba1081816e53565b780fc7b2d5f7ff45d
Kimberly Lowe-Williams <kimberlylowewilliams@gmail.com>
2015-06-19 00:22:54 -0500

    Removed quantity references from Orders Controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :user
   has_many :carted_products
 
-  validates :quantity, presence: true
-  validates :quantity, numericality: true
-  validates :quantity, greater_than: 0
+  
 end
